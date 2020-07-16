8eb48c5c1201f096607fe0270484ce47be04f53c
Anthony Gregis <anthony@thedevtron.com>
2020-07-16 17:24:48 -0500

    [Callback] Adds three functions that result in calculator using 2 callbacks

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ anotherGreeting = () => {
 }
 
 let myName = "Anthony"
-console.log(greeting(myName, anotherGreeting))
+//console.log(greeting(myName, anotherGreeting))
 
 printArray = (array, callback) => {
     for (let i = 0; i < array.length; i++) {
@@ -35,4 +35,24 @@ printArrayAgain = array => {
 
 const numbersArray = [1,2,3,4,5,6,7,0]
 
-printArray(numbersArray, printArrayAgain)
+//printArray(numbersArray, printArrayAgain)
+
+calculator = (num1, num2, callbackOne, callbackTwo) => {
+    let result = callbackOne(num1, num2) + callbackTwo(num1, num2)
+
+    return result;
+}
+
+addNumbers = (num1, num2) => {
+    let result = num1 + num2
+
+    return result;
+}
+
+subtractNumbers = (num1, num2) => {
+    let result = num1 - num2
+
+    return result;
+}
+
+console.log(calculator(5, 77, addNumbers, subtractNumbers))

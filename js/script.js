d266c4edaba9f19b4df00b89a826e768763a08d2
Anthony Gregis <anthony@thedevtron.com>
2020-07-16 17:19:26 -0500

    [Callback] Add printArray and printArrayAgain function

--- js/script.js
+++ js/script.js
@@ -14,3 +14,25 @@ anotherGreeting = () => {
 
 let myName = "Anthony"
 console.log(greeting(myName, anotherGreeting))
+
+printArray = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        let arrayElement = array[i]
+
+        console.log(arrayElement)
+    }
+
+    callback(array)
+}
+
+printArrayAgain = array => {
+    for (let i = 0; i < array.length; i++) {
+        let arrayElement = array[i]
+
+        console.log(arrayElement)
+    }
+}
+
+const numbersArray = [1,2,3,4,5,6,7,0]
+
+printArray(numbersArray, printArrayAgain)
